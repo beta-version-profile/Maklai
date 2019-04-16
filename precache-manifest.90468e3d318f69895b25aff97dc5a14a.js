0804cdcd909a4e7f082298deae7011db6fe8ca48
Kosianchuk Dmytro <kosianchuk.d@gmail.com>
2019-04-17 02:52:21 +0300

    Updates

---
+++ precache-manifest.90468e3d318f69895b25aff97dc5a14a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13bd4a23e2f89fc95bf3",
+    "url": "/Maklai/static/js/runtime~main.c38a20cb.js"
+  },
+  {
+    "revision": "164b0b2ed53cf1bf6d4e",
+    "url": "/Maklai/static/js/main.75d1ee9d.chunk.js"
+  },
+  {
+    "revision": "2ab9389885960996d914",
+    "url": "/Maklai/static/js/2.88771395.chunk.js"
+  },
+  {
+    "revision": "164b0b2ed53cf1bf6d4e",
+    "url": "/Maklai/static/css/main.3c9b46c6.chunk.css"
+  },
+  {
+    "revision": "d5d9166541eddee6aed7455705b42e0a",
+    "url": "/Maklai/index.html"
+  }
+];
